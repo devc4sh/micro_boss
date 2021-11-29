9bb13d1daddd6514b07fcc0902bfe37d2f361774
devc4sh <plra@kakao.com>
2021-11-30 01:26:19 +0900

    microservice boss

---
+++ main.py
@@ -0,0 +1,54 @@
+from flask import Flask, jsonify, abort
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+import requests
+from producer import publish
+from dataclasses import dataclass
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@db/main'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+CORS(app)
+
+db = SQLAlchemy(app)
+
+
+print('Running')
+
+@dataclass()
+class Shop(db.Model):
+    id: int
+    shop_name: str
+    shop_address: str
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    shop_name = db.Column(db.String(200))
+    shop_address = db.Column(db.String(200))
+
+@dataclass()
+class Order(db.Model):
+    id: int
+    shop: str
+    address: str
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    shop = db.Column(db.Integer)
+    address = db.Column(db.String(200))
+
+
+
+@app.route('/api/shop')
+def index():
+    return jsonify(Shop.query.all())
+
+@app.route('/api/order/<int:id>/deliver_finish', methods=['POST'])
+def deliverfinish(id):
+    publish('order_deliver_finish', id)
+    return jsonify({
+        'message':'success'
+    })
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
